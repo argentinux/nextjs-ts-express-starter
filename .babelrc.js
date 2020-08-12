ddd64391ac18616e4de840561235aca8234443a7
AT <>
2020-08-12 15:10:47 -0300

    transform runtime

--- .babelrc.js
+++ .babelrc.js
@@ -1,9 +1,6 @@
 const pkg = require('./package.json')
 
 module.exports = {
-  presets: [
-    'next/babel',
-    '@babel/preset-typescript',
-		'@babel/preset-env',
-	],
+  presets: ['next/babel', '@babel/preset-typescript', '@babel/preset-env'],
+  plugins: ['@babel/plugin-transform-runtime'],
 }
